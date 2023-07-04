fa831f49490c662c9d11d5d6072c111ac5bb5d81
liuliuhei <123804097+liuliuhei@users.noreply.github.com>
2023-07-04 18:32:18 +0800

    Update ex1a.py

--- debug/ex1a.py
+++ debug/ex1a.py
@@ -11,10 +11,7 @@ def histogram(x, xmin, xmax, histogram_out):
     stride = cuda.gridsize(1)
 
     for i in range(start, x.shape[0], stride):
-        bin_number = np.int32((x[i] - xmin)/bin_width)
-        if bin_number >= 0 and bin_number < histogram_out.shape[0]:
-            histogram_out[bin_number] += 1
-            print('in range', x[i], bin_number)
+        bin_number = np.int32
         else:
             print('out of range', x[i], bin_number)
 
